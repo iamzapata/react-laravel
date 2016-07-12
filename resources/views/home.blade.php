79ad6728eb3bcaf69a33c81107c14ddacea50a1a
Andres Zapata <me@azapata.io>
2016-07-11 22:30:42 -0500

    Work on crsf validation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,15 @@
             <title>Home</title>
             <meta name="description" content="">
             <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+            <script type="text/javascript">
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+            </script>
     
             <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
             
